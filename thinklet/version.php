e8a72c81aa42ff5796e887f940808799c9a9a6aa
odelhaye <odelhaye@gmail.com>
2026-09-16 13:54:45 +0300

    Clarify checkpoint messaging: update thinklet/version.php

--- thinklet/version.php
+++ thinklet/version.php
@@ -9,7 +9,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_thinklet';
-$plugin->version   = 2026052409;
+$plugin->version   = 2026052410;
 $plugin->requires  = 2022112800;
 $plugin->maturity  = MATURITY_BETA;
-$plugin->release   = '0.9.9 beta';
+$plugin->release   = '0.9.10 beta';
